a88c460b81c1a835040f99da5407d4749991ccf8
Marco Metz <marco.metz@gmail.com>
2021-11-25 18:43:53 +0100

    Update 20210616123650_create_survey_response_options.rb

--- db/migrate/20210616123650_create_survey_response_options.rb
+++ db/migrate/20210616123650_create_survey_response_options.rb
@@ -2,7 +2,7 @@ class CreateSurveyResponseOptions < ActiveRecord::Migration[5.2]
   def change
     create_table :survey_response_options do |t|
       t.integer :survey_question_id
-      t.longtext :title
+      t.text :title, limit: 16.megabytes - 1
       t.integer :votes_count, default: 0
 
       t.timestamps
