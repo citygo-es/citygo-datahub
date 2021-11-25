f19ef35f4772dcf63ab8102996b8e60f349323b3
Marco Metz <marco.metz@gmail.com>
2021-11-25 17:16:18 +0100

    Update 20190424153031_create_geo_locations.rb
    Remove index

--- db/migrate/20190424153031_create_geo_locations.rb
+++ db/migrate/20190424153031_create_geo_locations.rb
@@ -3,7 +3,7 @@ class CreateGeoLocations < ActiveRecord::Migration[5.2]
     create_table :geo_locations do |t|
       t.float :latitude
       t.float :longitude
-      t.references :geo_locateable, polymorphic: true, index: "index_geo_locateable_type_and_id"
+      t.references :geo_locateable, polymorphic: true
       t.timestamps
     end
   end
