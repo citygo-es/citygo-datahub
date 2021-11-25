6aaa7ebaa0090c4802b44f9cbfb79c54c648e98c
Marco Metz <marco.metz@gmail.com>
2021-11-25 18:13:50 +0100

    Update 20201203145226_fix_precision_in_geo_locations.rb
    Double gibt es nicht in Postgres

--- db/migrate/20201203145226_fix_precision_in_geo_locations.rb
+++ db/migrate/20201203145226_fix_precision_in_geo_locations.rb
@@ -1,6 +1,6 @@
 class FixPrecisionInGeoLocations < ActiveRecord::Migration[5.2]
   def change
-    change_column :geo_locations, :latitude, :double
-    change_column :geo_locations, :longitude, :double
+    change_column :geo_locations, :latitude, :float, precision: 64, scale: 16
+    change_column :geo_locations, :longitude, :float, precision: 64, scale: 16
   end
 end
