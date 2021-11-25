a635330f7846dfa9acdecec3a6f08af7b235b87e
Marco Metz <marco.metz@gmail.com>
2021-11-25 18:25:59 +0100

    Update 20210616115921_create_survey_polls.rb

--- db/migrate/20210616115921_create_survey_polls.rb
+++ db/migrate/20210616115921_create_survey_polls.rb
@@ -1,8 +1,8 @@
 class CreateSurveyPolls < ActiveRecord::Migration[5.2]
   def change
     create_table :survey_polls do |t|
-      t.longtext :title
-      t.longtext :description
+      t.text :title, limit: 4294967295
+      t.text :description, limit: 4294967295
 
       t.timestamps
     end
