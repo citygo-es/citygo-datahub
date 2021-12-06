00a8a86286143a2b6fcc6e529dae39f007389ce2
Marco Metz <mm@tpwd.de>
2021-12-06 19:57:52 +0100

    CrossOrigin Fix nur beim Exporter Controller
    Unter Umständen sorgt der CrossOrigin Header dafür, dass die User/sign_in API für das CMS nicht mehr funktioniert.
    Daher wird der Header nun erstmal nur bei dem Exporter hinzugefügt.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,19 +3,7 @@
 class ApplicationController < ActionController::Base
   layout "doorkeeper/application"
 
-  after_action :set_headers
-
   def generate_204
     head(:no_content)
   end
-
-  def set_headers
-    headers["Access-Control-Allow-Origin"] = "*"
-    headers["Access-Control-Request-Method"] = %w{GET POST OPTIONS}.join(",")
-
-    # possible X-Frame-Options: https://developer.mozilla.org/de/docs/Web/HTTP/Headers/X-Frame-Options
-    # Rails sets "sameorigin" as default, so we need to remove that in order to allow all
-    headers.delete "X-Frame-Options"
-  end
-
 end
