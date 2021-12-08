f80868e3b0c8ea43b3cd3891229f54ca2caec7ca
Marco Metz <mm@tpwd.de>
2021-12-08 12:48:00 +0100

    Performance and crossside Scripting
    - reenabled headers for crossside access on all controllers
    - trying nginx configs for zipping transfered data

--- app/controllers/export/point_of_interests_controller.rb
+++ app/controllers/export/point_of_interests_controller.rb
@@ -1,17 +1,8 @@
 class Export::PointOfInterestsController < ApplicationController
-  after_action :set_headers
+
   # action_cache
 
   def index
     @entries = PointOfInterest.joins(location: :geo_location).visible
-  end
-
-  def set_headers
-    headers["Access-Control-Allow-Origin"] = "*"
-    headers["Access-Control-Request-Method"] = %w{GET POST OPTIONS}.join(",")
-
-    # possible X-Frame-Options: https://developer.mozilla.org/de/docs/Web/HTTP/Headers/X-Frame-Options
-    # Rails sets "sameorigin" as default, so we need to remove that in order to allow all
-    headers.delete "X-Frame-Options"
   end
 end
