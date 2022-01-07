4e576e19deed655cee0afcb21dd537406da35508
Marco Metz <mm@tpwd.de>
2022-01-07 10:27:29 +0100

    Preview of SVG added
    - Category has link to Show-Page
    - Show-Page now has a preview of the svg icon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,6 +20,7 @@ module ApplicationHelper
       tree_element << content_tag("span", "ID:#{category.id}", class: "badge badge-info")
       tree_element << category.name
       element_buttons << link_to("New Child", new_category_path(parent_id: category.id), class: "btn btn-xs btn-outline-success")
+      element_buttons << link_to("Show", category_path(category), class: "btn btn-xs btn-outline-secondary")
       element_buttons << link_to("Edit", edit_category_path(category), class: "btn btn-xs btn-outline-secondary")
       element_buttons << link_to("Destroy", category, method: :delete, data: { confirm: "Are you sure? All children are destroyed as well!" }, class: "btn btn-xs btn-outline-danger")
       tree_element << content_tag("div", raw(element_buttons.join), class: "action-links")
