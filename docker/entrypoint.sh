0d6a8c1ac216b62454bd897c9f73415e7b2f993b
Volker Kerkhoff <volker_kerkhoff@epam.com>
2023-12-14 09:36:35 +0100

    Mas cosas: - Quitar llamada a npm en entrypoint.sh - Commit de manifiestos de Kubernetes

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,7 +6,7 @@ DB=${DB_HOST:-bbnavi-datahub-postgresql:5432}
 
 dockerize -wait tcp://$DB -timeout 30s
 
-npm set audit false
+# npm set audit false
 bundle exec rake db:gis:setup
 bundle exec rake db:migrate
 
